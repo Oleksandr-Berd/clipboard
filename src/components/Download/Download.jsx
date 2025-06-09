f820b536d58a839cb4dd4f17171c2bb8e09db498
Oleksandr-Berd <alex.berd86@gmail.com>
2025-06-09 21:18:21 +0300

    shared layout is done

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -1,15 +1,17 @@
+import * as SC from "./DownloadStyled"
+
 import DownloadLinks from "../DownloadLinks/DownloadLinks";
 
 const Download = () => {
   return (
-    <div>
+    <SC.DownloadStyled>
       <h3>Clipboard for iOS and Mac OS</h3>
       <p>
         Available for free on the App Store. Download for Mac or iOS, sync with
         iCloud and you’re ready to start adding to your clipboard.
       </p>
       <DownloadLinks />
-    </div>
+    </SC.DownloadStyled>
   );
 };
 
---
+++ src/components/Download/DownloadStyled.js
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+
+export const DownloadStyled = styled.div`
+  padding-top: 64px;
+  padding-bottom: 170px;
+
+  text-align: center;
+
+  & > h3 {
+    margin-bottom: 16px;
+
+    font-size: 28px;
+    font-weight: 600;
+    letter-spacing: -0.46px;
+
+    color: #4C545C;
+  }
+
+  & > p{
+    margin-bottom: 48px;
+
+    font-size: 16px;
+    line-height: 1.6;
+    letter-spacing: 0.12px;
+
+    color: #9EABB2;
+  }
+`;
