75e9d26d96cb0620e6bf93d1225e69c972b9b134
Oleksandr-Berd <alex.berd86@gmail.com>
2025-06-10 08:37:53 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['@emotion/styled']
-    }
-  },
   plugins: [react()],
 });
