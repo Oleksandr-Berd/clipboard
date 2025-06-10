55f4b69e886d6b4371f15fb1e0d54821ce8a745b
Oleksandr-Berd <alex.berd86@gmail.com>
2025-06-10 07:36:47 +0300

    access desk is done

--- src/components/Access/Access.jsx
+++ src/components/Access/Access.jsx
@@ -4,11 +4,13 @@ import devicesImg from "../../assets/images/image-devices.png"
 const Access = () => {
   return (
     <SC.AccessStyled>
+      <SC.AccessContentCon>
       <SC.AccessTitleStyled>Access Clipboard Anywhere</SC.AccessTitleStyled>
       <SC.AccessContentStyled>
         Whether you’re on the go, or at your computer, you can access all your
         Clipboard snippets in a few simple clicks.
       </SC.AccessContentStyled>
+      </SC.AccessContentCon>
       <SC.AccessThumbStyled>
         <img src={devicesImg} alt="devices" />
       </SC.AccessThumbStyled>
--- src/components/Access/AccessStyled.js
+++ src/components/Access/AccessStyled.js
@@ -1,40 +1,70 @@
 import styled from "@emotion/styled";
 
 export const AccessStyled = styled.div`
-padding-top: 92px;
-padding-bottom: 64px;
+  padding-top: 92px;
+  padding-bottom: 64px;
 
-text-align: center;
+  text-align: center;
+
+  @media (min-width: 1280px) {
+    padding-top: 72px;
+    padding-bottom: 80px;
+  }
+`;
+
+export const AccessContentCon = styled.div`
+  margin-bottom: 64px;
+
+  @media (min-width: 1280px) {
+    width: 50%;
+
+    margin-left: auto;
+    margin-right: auto;
+  }
 `;
 
 export const AccessTitleStyled = styled.h3`
-margin-bottom: 16px;
+  margin-bottom: 16px;
 
-font-size: 28px;
-font-weight: 600;
-letter-spacing: -0.46px;
+  font-size: 28px;
+  font-weight: 600;
+  letter-spacing: -0.46px;
 
-color: #4C545C;
-`
+  color: #4c545c;
+
+  @media (min-width: 1280px) {
+    margin-bottom: 20px;
+
+    font-size: 36px;
+    letter-spacing: -0.59px;
+  }
+`;
 
 export const AccessContentStyled = styled.p`
-margin-bottom: 64px;
+  font-size: 16px;
+  line-height: 1.6;
+  letter-spacing: 0.12px;
 
-font-size: 16px;
-line-height: 1.6;
-letter-spacing: 0.12px;
+  color: #9eabb2;
 
-color: #9EABB2;
-`
+  @media (min-width: 1280px) {
+    font-size: 18px;
+  }
+`;
 
 export const AccessThumbStyled = styled.div`
-display: inline-block;
+  display: inline-block;
 
-width: 311px;
-height: 182px;
+  width: 311px;
+  height: 182px;
 
-& >img{
+  @media (min-width: 1280px) {
+    width: 812px;
+    height: 475px;
+  }
+
+  & > img {
     width: inherit;
     height: inherit;
-}
-`
+  }
+`;
